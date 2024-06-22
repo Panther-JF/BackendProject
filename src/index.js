63c82e25542599926d8a13c2777feecbcf9d86a4
Panther-JF <itsjayesh.tech@gmail.com>
2024-06-22 18:54:11 +0530

    add util to codebase

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import express from "express"
 
 dotenv.config({
     path:'./.env'
 })
 // "-r dotenv/config --experimental-json-modules" is written in script in package.json file so that improt dotenv can work properly.
+const app=express()
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERRR: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port : ${process.env.PORT}`);
+    })
+    
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
